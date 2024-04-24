4f0809b43408795d643bc87c5694d853bbd15dbe
alair7396 <165728217+alair7396@users.noreply.github.com>
2024-04-23 21:55:47 -0300

    Javascript
    aulas de javascript

---
+++ First.js
@@ -0,0 +1,20 @@
+let idade_pessoa = 10
+let nome_pessoa = 'alair'
+let possui_ebook = false
+let endereço_pessoa
+let numero1, numero2, somanumeros
+//entrada de dados
+numero1=Number(prompt('Digite um numero: '))
+numero2=Number(prompt('Digite um numero: '))
+//calculos e logica
+somanumeros=numero1+numero2
+
+
+
+
+
+
+//saida de dados
+alert(somanumeros)
+
+alert('numero digitado foi:\n'+numero1)
